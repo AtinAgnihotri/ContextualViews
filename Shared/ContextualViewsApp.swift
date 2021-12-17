db3e47ff128b629901d1f2b9129da20d5cdc5be8
Atin Agnihotri <atinagnihotri@gmail.com>
2021-12-17 19:34:47 +0530

    Initial Commit

---
+++ Shared/ContextualViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextualViewsApp.swift
+//  Shared
+//
+//  Created by Atin Aganihotri on 17/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ContextualViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
