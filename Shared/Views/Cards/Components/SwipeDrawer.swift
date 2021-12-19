768ed7cd3eb6eccf9c8715ddb5c690523546aaf5
Atin Agnihotri <atinagnihotri@gmail.com>
2021-12-19 18:25:56 +0530

    Extracts SwipeDrawer from BigDisplayCard

---
+++ Shared/Views/Cards/Components/SwipeDrawer.swift
@@ -0,0 +1,20 @@
+//
+//  BigCardSwipeDrawer.swift
+//  ContextualViews
+//
+//  Created by Atin Aganihotri on 19/12/21.
+//
+
+import SwiftUI
+
+struct BigCardSwipeDrawer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BigCardSwipeDrawer_Previews: PreviewProvider {
+    static var previews: some View {
+        BigCardSwipeDrawer()
+    }
+}
