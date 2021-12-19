a02a8ccca300e8ec47e6eff405eb167ebe4a2e30
Atin Agnihotri <atinagnihotri@gmail.com>
2021-12-19 16:05:16 +0530

    Adds Loader and LaunchScreen

---
+++ Shared/Views/Cards/Components/CVLoader.swift
@@ -0,0 +1,20 @@
+//
+//  CVLoader.swift
+//  ContextualViews
+//
+//  Created by Atin Aganihotri on 19/12/21.
+//
+
+import SwiftUI
+
+struct CVLoader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CVLoader_Previews: PreviewProvider {
+    static var previews: some View {
+        CVLoader()
+    }
+}
