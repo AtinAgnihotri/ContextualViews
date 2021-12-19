e0eb91499d44e62393fa3f68eda5a8f826274b9a
Atin Agnihotri <atinagnihotri@gmail.com>
2021-12-19 15:08:19 +0530

    Adds Error Messages

---
+++ Shared/Views/Cards/Components/SwipeActionButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  SwipeActionButtonView.swift
+//  ContextualViews
+//
+//  Created by Atin Aganihotri on 19/12/21.
+//
+
+import SwiftUI
+
+struct SwipeActionButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwipeActionButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwipeActionButtonView()
+    }
+}
